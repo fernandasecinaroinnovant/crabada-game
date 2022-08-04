364618ca21fc2f73d4b46fef250b6d5dc41af9cd
fernandasecinaroinnovant <91079329+fernandasecinaroinnovant@users.noreply.github.com>
2022-08-04 17:08:03 -0300

    sidebar

--- crabada/src/components/Sidebar/Sidebar.tsx
+++ crabada/src/components/Sidebar/Sidebar.tsx
@@ -15,38 +15,47 @@ const sidebarOptions: (
   | {
       component: JSX.Element;
       name?: undefined;
+      id: string;
     }
   | {
       component: JSX.Element;
       name: string;
+      id: string;
     }
 )[] = [
   {
     component: <P2eIcon />,
+    id: "companyLogo",
   },
   {
     component: <MagnifyingGlass />,
     name: "Search",
+    id: "search",
   },
   {
     component: <Applications />,
     name: "Home",
+    id: "home",
   },
   {
     component: <Game />,
     name: "Games",
+    id: "games",
   },
   {
     component: <Compass />,
     name: "News",
+    id: "news",
   },
   {
     component: <Diamond />,
     name: "Careers",
+    id: "careers",
   },
   {
     component: <AboutIcon />,
     name: "About",
+    id: "about",
   },
 ];
 
@@ -69,8 +78,8 @@ const Sidebar: React.FC<{}> = () => {
       onMouseLeave={onMouseLeaveHandler}
     >
       <div className="sidebar-content">
-        {sidebarOptions.map(({ component, name }) => (
-          <div className="icon-container">
+        {sidebarOptions.map(({ component, name, id }) => (
+          <div key={id} className="icon-container">
             <span className="logo">{component}</span>
             {isHoveringOnSidebar && name && <span>{name}</span>}
           </div>
