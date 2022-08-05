57153e00f7c3a3c8f1431fc747f8e3f62325d3e3
fernandasecinaroinnovant <91079329+fernandasecinaroinnovant@users.noreply.github.com>
2022-08-04 22:43:41 -0300

    layout

--- crabada/src/pages/Layout/Layout.tsx
+++ crabada/src/pages/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../../components/Sidebar/Sidebar";
-import Footer from "../../components/Footer/Footer";
+import Sidebar from "../../components/Common/Sidebar/Sidebar";
+import Footer from "../../components/Common/Footer/Footer";
 import "./Layout.scss";
 
 const Layout: React.FC<{}> = () => {
